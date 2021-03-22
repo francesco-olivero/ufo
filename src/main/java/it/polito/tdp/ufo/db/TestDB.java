945705b775b0fa9f2ea3e90495ceba448fe97385
francesco-olivero <75547878+francesco-olivero@users.noreply.github.com>
2021-03-22 09:32:16 +0100

    new connection

--- src/main/java/it/polito/tdp/ufo/db/TestDB.java
+++ src/main/java/it/polito/tdp/ufo/db/TestDB.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.ufo.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestDB {
+
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost?user=root&password=30Franci499";
+		
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+
+	}
 
 }
