4b2d17b7bff83def9ce0dcb16e549b8eff7fb5b7
francesco-olivero <75547878+francesco-olivero@users.noreply.github.com>
2021-03-22 09:45:35 +0100

    till passage 4

--- src/main/java/it/polito/tdp/ufo/db/TestDB.java
+++ src/main/java/it/polito/tdp/ufo/db/TestDB.java
@@ -3,6 +3,7 @@ package it.polito.tdp.ufo.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestDB {
 
@@ -10,7 +11,16 @@ public class TestDB {
 		String jdbcURL = "jdbc:mysql://localhost?user=root&password=30Franci499";
 		
 		try {
-			Connection connection = DriverManager.getConnection(jdbcURL);
+			Connection conn = DriverManager.getConnection(jdbcURL);
+			
+			Statement st = conn.createStatement();
+			
+			String sql = "select distinct shape"
+					+ "from sighting"
+					+ "where shape<>''"
+					+ "order by shape ASC";
+			
+			conn.close(); // sistema sopporta solo x connesioni, sempre chiuderle
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} 
